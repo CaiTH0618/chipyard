3e195da8350cb4b3361c796e0172d4e7c0752bc6
Cai Tianhao <caith1234@163.com>
2025-11-05 14:32:08 +0800

    remove config GemminiWithSpad

--- generators/chipyard/src/main/scala/config/GemminiSoCConfigs.scala
+++ generators/chipyard/src/main/scala/config/GemminiSoCConfigs.scala
@@ -25,22 +25,3 @@ class CustomGemminiSoCConfig extends Config(
   new chipyard.config.WithSystemBusWidth(GemminiCustomConfigs.customConfig.dma_buswidth) ++
   new chipyard.config.AbstractConfig
 )
-
-class GemminiWithSpad extends Config(
-  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
-
-  new testchipip.soc.WithMbusScratchpad(base = 0xC0000000L, size = 4 << 20) ++
-
-  new gemmini.GemminiCustomConfig ++
-
-  new chipyard.config.WithL2TLBs(512) ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache(
-    nWays = 8,
-    capacityKB = 512,
-    outerLatencyCycles = 40,
-    subBankingFactor = 4
-  ) ++
-  new chipyard.CustomGemmminiCPUConfigs.CustomCPU(1) ++
-  new chipyard.config.WithSystemBusWidth(GemminiCustomConfigs.customConfig.dma_buswidth) ++
-  new chipyard.config.AbstractConfig
-)
