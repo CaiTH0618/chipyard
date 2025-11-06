aa18036910db87d297c75cff4e3c8062e105575c
Cai Tianhao <caith1234@163.com>
2025-11-06 16:34:12 +0800

    modified scratchpad parameters

--- generators/chipyard/src/main/scala/config/GemminiLearningConfigs.scala
+++ generators/chipyard/src/main/scala/config/GemminiLearningConfigs.scala
@@ -1,6 +1,8 @@
 package chipyard
 
 import org.chipsalliance.cde.config.Config
+import freechips.rocketchip.subsystem.{MBUS, SBUS}
+import freechips.rocketchip.diplomacy.BufferParams
 
 
 class GemminiLearningConfigBasic extends Config(
@@ -19,9 +21,26 @@ class GemminiLearningConfigWithScratchpad extends Config(
   // Improve sim speed by removing TileLink monitors
   new freechips.rocketchip.subsystem.WithoutTLMonitors ++
 
+  // Add a Scratchpad to system bus
+  new testchipip.soc.WithScratchpad(
+    busWhere = SBUS,
+    base = 0xC0000000L, 
+    size = 1 << 20,  // 1MB
+    banks = 4,
+    subBanks = 1,
+    buffer = BufferParams(8),
+    outerBuffer = BufferParams(8),
+  ) ++
   // Add a Scratchpad to memory bus
-  new testchipip.soc.WithSbusScratchpad(base = 0xC0000000L, size = 1 << 20) ++
-  new testchipip.soc.WithMbusScratchpad(base = 0x08000000L, size = 1 << 20) ++
+  new testchipip.soc.WithScratchpad(
+    busWhere = MBUS,
+    base = 0x08000000L, 
+    size = 1 << 20,  // 1MB
+    banks = 4,
+    subBanks = 1,
+    buffer = BufferParams(8),
+    outerBuffer = BufferParams(8),
+  ) ++
   // Remove the default Scratchpad in `AbstractConfig`
   new testchipip.soc.WithNoScratchpads() ++
 
